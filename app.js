f0734289d5bd87fc80699064fcf94e1af911af24
Chris Sturm <casturm@gmail.com>
2014-04-16 09:59:40 -0700

    refactor app.js to look more like ui-router sample

--- app.js
+++ app.js
@@ -1,49 +1,84 @@
-var app = angular.module('app', ['ui.router']);
+angular.module('rdx', [
+  //'rdx.cases.service',
+  'rdx.cases.controller',
+  'rdx.cases',
+  'ui.router'
+])
 
-app.controller('MainCtrl', ['$scope', '$state', function($scope, $state) {
-  $scope.save = function(isValid) {
-    if (isValid) {
-      alert('save');
-      $state.go('step2');
+.run(
+  ['$rootScope', '$state', '$stateParams',
+    function ($rootScope,   $state,   $stateParams) {
+
+      // It's very handy to add references to $state and $stateParams to the $rootScope
+      // so that you can access them from any scope
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
     }
-  };
-}]);
+  ]
+)
 
-app.factory('Cases', ['$http', function($http) {
-  return {
-    post: function() {
-      alert('post');
-//      $http.post('/api/case', $scope.userForm.fields);
-    }
-  };
-}]);
-
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/home');
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
 
   $stateProvider
     .state('home', {
-      url: '/home',
+      url: '/',
       templateUrl: 'partial-home.html'
     })
 
-    .state('step1', {
-      url: '/step1',
-      templateUrl: 'partial-step1.html',
-      controller: 'MainCtrl'
-    })
-
-    .state('step2', {
-      url: '/step2',
-      templateUrl: 'partial-step2.html',
-      controller: 'MainCtrl'
-    })
-
-    .state('step3', {
-      url: '/step3',
-      template: "<div class'jumbotron'>Thank You!</div>"
-    })
-
     .state('about', {
+      url: '/about',
+      template: '<div class="jumbotron text-center"><h1>It\'s about Life Insurance</h1></div>'
     });
 }]);
+
+angular.module('rdx.cases.controller', [
+])
+
+.controller('interview', ['$scope', function($scope) {
+  console.log("init interview controller");
+
+  $scope.save = function(isValid, nextStep) {
+    $scope.submitted = true;
+
+    if (isValid) {
+      //cases.post();
+      $scope.submitted = false;
+      $scope.$state.go(nextStep);
+    }
+  };
+}]);
+
+
+angular.module('rdx.cases', [
+  'ui.router'
+])
+
+.config(['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('interview', {
+        abstract: true,
+        url: '/interview',
+        templateUrl: 'interview/interview.html',
+        controller: 'interview'
+      })
+
+      .state('interview.step1', {
+        url: '',
+        templateUrl: 'interview/step1.html'
+      })
+
+      .state('interview.step2', {
+        url: '',
+        templateUrl: 'interview/step2.html'
+      })
+
+      .state('interview.thankyou', {
+        url: '',
+        template: '<div class="jumbotron text-center"><h1>Thank You!</h1><p>Your loved ones will thank you</p></div>'
+      });
+}]);
+
+
