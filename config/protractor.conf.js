94e06d61973dc69a9eb3f3777a1b61662d20b02f
Chris Sturm <casturm@gmail.com>
2014-05-07 17:35:03 -0700

    make e2e tests work more reliably

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -10,24 +10,40 @@ exports.config = {
   },
 
   suites: {
-    interview: '../test/e2e/interview.js',
-    cases: '../test/e2e/cases.js'
+    cases: '../test/e2e/cases.js',
+    interview: '../test/e2e/interview.js'
   },
 
   // Options to be passed to Jasmine-node.
+  // https://github.com/angular/protractor/blob/master/referenceConf.js
   jasmineNodeOpts: {
-    showColors: true, // Use colors in the command line report.
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    isVerbose: true,
+    includeStackTrace: true
   },
 
   baseUrl: baseUrl,
 
+  // This can be changed via the command line as:
+  // --params.login.user 'ngrocks'
+  params: {
+    login: {
+      user: 'admin',
+      password: 'doit'
+    }
+  },
+
   onPrepare: function() {
+    browser.driver.manage().window().setSize(998, 800);
+
     browser.driver.get(baseUrl + '/#/login');
 
     browser.driver.wait(function() {
       return browser.driver.findElement(by.name('username')).isDisplayed();
     }, 1000);
 
+    // can't access params here ;(
     browser.driver.findElement(by.name('username')).sendKeys('admin');
     browser.driver.findElement(by.name('password')).sendKeys('doit');
     browser.driver.findElement(by.id('login-button')).click();
--- test/e2e/cases.js
+++ test/e2e/cases.js
@@ -2,52 +2,52 @@ describe('AngularRDX', function() {
 
   var ptor;
   beforeEach(function() {
+//    browser.get('/');
     ptor = protractor.getInstance();
+
+    element(by.css('.navbar ul li')).click().then(function() {
+      expect(ptor.isElementPresent(by.css('.page-header'))).toBe(true);
+    });
   });
 
   describe('cases page', function() {
+    var name, email;
+
     beforeEach(function() {
-      element(by.css('.navbar ul li')).click().then(function() {
-        expect(ptor.isElementPresent(by.css('.page-header'))).toBe(true);
+      var nameElement = element(by.id('cases-content')).findElement(by.repeater('c in cases').row(0).column('c.name'));
+      var emailElement = element(by.id('cases-content')).findElement(by.repeater('c in cases').row(0).column('c.email'));
+      name = nameElement.getText();
+      email = emailElement.getText();
+      nameElement.click();
+    });
+
+    it('should display the detail when the name is clicked on', function () {
+      element(by.id('case-detail')).findElements(by.binding('case.name')).then(function(names) {
+        expect(names[0].getText()).toEqual(name);
+        expect(names[1].getText()).toEqual(name);
       });
     });
 
-    it('should display the list of cases when I click the Cases menu item', function() {
-      expect(element(by.repeater('c in cases').row(0).column('c.name')).getText()).toEqual('bob');
+    it('should display the name item edit view when I click on the name item ', function() {
+      element(by.id('case-detail')).findElements(by.binding('case.name')).then(function(names) {
+        expect(names[0].getText()).toEqual(name);
+        names[1].click();
+      });
+      element.all(by.binding('item')).then(function(arr) {
+        expect(arr[0].getText()).toEqual('name Edit');
+        expect(arr[1].getText()).toEqual(name);
+      });
     });
 
-    describe('case detail', function() {
-      beforeEach(function() {
-        element(by.repeater('c in cases').row(0).column('c.name')).click();
+    it('should display the email item edit view when I click on the email item ', function() {
+      element.all(by.binding('case.email')).then(function(names) {
+        names[0].click();
       });
-
-      it('should display the detail when the name is clicked on', function () {
-        element.all(by.binding('case.name')).then(function(arr) {
-          expect(arr[0].getText()).toEqual('bob');
-          expect(arr[1].getText()).toEqual('bob');
-        });
+      element.all(by.binding('item')).then(function(arr) {
+        expect(arr[0].getText()).toEqual('email Edit');
+        expect(arr[1].getText()).toEqual(email);
       });
-
-      it('should display the name item edit view when I click on the name item ', function() {
-        element.all(by.binding('case.name')).then(function(names) {
-          names[2].click();
-        });
-        element.all(by.binding('item')).then(function(arr) {
-          expect(arr[0].getText()).toEqual('name Edit');
-          expect(arr[1].getText()).toEqual('bob');
-        });
-      });
-
-      it('should display the email item edit view when I click on the email item ', function() {
-        element.all(by.binding('case.email')).then(function(names) {
-          names[0].click();
-        });
-        element.all(by.binding('item')).then(function(arr) {
-          expect(arr[0].getText()).toEqual('email Edit');
-          expect(arr[1].getText()).toEqual('bob@foobar.com');
-        });
-      });
-    })
-  });
+    });
+  })
 });
 
